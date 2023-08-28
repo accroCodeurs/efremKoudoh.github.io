328e499b1d68cec39b7c8409c798b32865c312e4
accroCodeurs <139649016+accroCodeurs@users.noreply.github.com>
2023-08-28 22:42:59 +0200

    commit

--- vite.config.js
+++ vite.config.js
@@ -1,15 +1,7 @@
-// vite.config.js
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  build: {
-    rollupOptions: {
-      output: {
-        format: 'cjs', // Désactiver le module
-      },
-    },
-  },
 })
